6bbf208922734d5c58507dda7010f5316b891c43
Maunk <git config --global user.email>
2017-02-05 15:04:43 +0100

    Added initial project structure.

---
+++ src/main/java/de/maunk/cocktailr/web/CocktailrApplication.java
@@ -0,0 +1,12 @@
+package de.maunk.cocktailr.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocktailrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CocktailrApplication.class, args);
+	}
+}
